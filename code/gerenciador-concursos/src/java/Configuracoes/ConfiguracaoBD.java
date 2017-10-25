c84d0acb4da9d9e2ae0d53be7efc6557b9813488
Andre Rodrigues <andre.002008@msn.com>
2017-10-24 22:52:52 -0200

    Classe de configuracao de BD

---
+++ code/gerenciador-concursos/src/java/Configuracoes/ConfiguracaoBD.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Configuracoes;
+
+/**
+ *
+ * @author André Rodrigues
+ */
+public class ConfiguracaoBD {
+    public static final String USUARIO = "";
+    private static final String SENHA = "";
+    private static final String URL = "";
+    private static final String DRIVER = "";
+}
