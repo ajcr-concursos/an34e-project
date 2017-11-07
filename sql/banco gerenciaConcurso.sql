105d0414cc1e45e00199f95aa05a9016a1ab57d3
JeanBalarin <jfrancisco@alunos.utfpr.edu.br>
2017-11-07 21:53:54 -0200

    sql

--- sql/banco gerenciaConcurso.sql
+++ sql/banco gerenciaConcurso.sql
@@ -22,10 +22,12 @@ create table area_candidato(
 	foreign key (id_area) references area(id)
 );
 
-CREATE TABLE INSTITUIÇÃO(
-    ID INT AUTO_INCREMENT PRIMARY KEY,
-	NOME VARCHAR(45) NOT NULL,
-	CNPJ VARCHAR(16)UNIQUE NOT NULL
+create table instituicao(
+	id int auto_increment primary key,
+	nome varchar(45) not null,
+	email varchar(45) unique not null,
+	senha varchar(20) not null,
+	cnpj varchar(16) unique not null
 );
 
 CREATE TABLE CONCURSO(
@@ -50,39 +52,3 @@ CREATE TABLE AREA_CONCURSO(
 	FOREIGN KEY (ID_AREA) REFERENCES AREA(ID),
 	FOREIGN KEY (ID_CONCURSO) REFERENCES CONCURSO(ID)
 );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-=======
-create table instituicao(
-	id int auto_increment primary key,
-	nome varchar(45) not null,
-	email varchar(45) unique not null,
-	senha varchar(20) not null,
-	cnpj varchar(16) unique not null
-);
-
-create table resultado(
-	
-);
->>>>>>> 7dc6542342a8db315b13e2c91199fadbf976848c
