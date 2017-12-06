69ff6482aa01bbc517523ef2e7693ac867173b9a
JeanBalarin <jfrancisco@alunos.utfpr.edu.br>
2017-12-06 21:40:19 -0200

    ajustes conflitos

--- code/gerenciador-concursos/src/java/Model/BuscarConcursoDAO.java
+++ code/gerenciador-concursos/src/java/Model/BuscarConcursoDAO.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -72,39 +71,3 @@ public class BuscarConcursoDAO {
         }
     }
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Model;
-
-import BD.BancoDados;
-import java.sql.Connection;
-import java.util.ArrayList;
-import java.util.List;
-
-/**
- *
- * @author Jean
- */
-public class BuscarConcursoDAO {
-    private Connection con;
-    public BuscarConcursoDAO(){
-        con = new BancoDados().getConnection();
-    }
-    /*
-    public List<Concurso> select(){
-        List<Concurso> lst = new ArrayList<>();
-        String sql = "select * from concurso";
-        try{
-            
-        }catch(Exception err){
-            System.out.println(err.getMessage());
-        }
-        return null;
-    }
-*/
-}
->>>>>>> d42c10b653821791f4bbf008f91e4c4bde62d47c
